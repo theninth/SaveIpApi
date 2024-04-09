d3a186ccf8f513e08958101ef3af1a2b2c11f333
thenajnth <thenajnth@gmail.com>
2024-04-09 07:38:02 +0200

    Removed swagger

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddTransient<DataContext>();
 builder.Services.AddTransient<IpAddressesRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
@@ -24,11 +23,6 @@ builder.Logging.AddSerilog();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 app.UseHttpsRedirection();
 
 app.MapGet("ip/{key}/", async (IpAddressesRepository repository, string key) =>
@@ -40,16 +34,12 @@ app.MapGet("ip/{key}/", async (IpAddressesRepository repository, string key) =>
     }
     return Results.NotFound();
 })
-.WithName("GetLatestIp")
-.WithOpenApi()
 .AddEndpointFilter<ApiKeyAuthenticationEndpointFilter>();
 
 app.MapPost("ip/{key}/", async (IpAddressesRepository repository, [FromBody]IpRequest model, string key) =>
 {
     await repository.Create(model.ToIpAdressEntity(key));
 })
-.WithName("UpdateIp")
-.WithOpenApi()
 .AddEndpointFilter<ApiKeyAuthenticationEndpointFilter>();
 
 var context = app.Services.GetRequiredService<DataContext>();
