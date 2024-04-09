b2f7ca7ade1ab8c7b0b91edb86cb2134d0e1fc97
thenajnth <thenajnth@gmail.com>
2024-04-09 19:15:04 +0200

    Only update database with ip when new ip is different from old ip.

--- src/Repositories/IpAddressesRepository.cs
+++ src/Repositories/IpAddressesRepository.cs
@@ -11,20 +11,21 @@ public class IpAddressesRepository(DataContext context)
     public async Task<GetIpResponse?> GetLatest(string key)
     {
         using var connection = _context.CreateConnection();
-        var sql =
-            """
-                SELECT IpAddress AS Ip, RecievedDate FROM IpAddresses WHERE Key = @Key ORDER BY RecievedDate DESC LIMIT 1
-            """;
+        var sql = "SELECT IpAddress AS Ip, RecievedDate FROM IpAddresses WHERE Key = @Key ORDER BY RecievedDate DESC LIMIT 1";
         return await connection.QueryFirstOrDefaultAsync<GetIpResponse>(sql, new { Key = key });
     }
 
     public async Task Create(IpAdressEntity ipAdress)
     {
         using var connection = _context.CreateConnection();
-        var sql =
-            """
-                INSERT INTO IpAddresses (RecievedDate, Key, IpAddress) VALUES (@RecievedDate, @Key, @IpAddress);
-            """;
-        await connection.ExecuteAsync(sql, ipAdress);
+
+        var sql = "SELECT IpAddress AS Ip FROM IpAddresses WHERE Key = @Key ORDER BY RecievedDate DESC LIMIT 1";
+        var latestIp = await connection.QueryFirstOrDefaultAsync<string>(sql, new { ipAdress.Key });
+
+        if (latestIp != ipAdress.IpAddress)
+        {
+            sql = "INSERT INTO IpAddresses (RecievedDate, Key, IpAddress) VALUES (@RecievedDate, @Key, @IpAddress);";
+            await connection.ExecuteAsync(sql, ipAdress);
+        }
     }
 }
